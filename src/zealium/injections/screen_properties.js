baed5d36cd7a34dc66a7549c77b0f058f2e6e448
Renks <renks@localhost.localdomain>
2025-07-18 08:45:57 -0300

    feat: neutral lang

--- src/zealium/injections/screen_properties.js
+++ src/zealium/injections/screen_properties.js
@@ -1,9 +1,12 @@
 (() => {
-    Object.defineProperty(window.screen, 'width', { get: () => 1920 });
-    Object.defineProperty(window.screen, 'height', { get: () => 1080 });
-    Object.defineProperty(window.screen, 'availWidth', { get: () => 1920 });
-    Object.defineProperty(window.screen, 'availHeight', { get: () => 1040 });
-    
-    Object.defineProperty(window, 'outerWidth', { get: () => window.innerWidth + 100 });
-    Object.defineProperty(window, 'outerHeight', { get: () => window.innerHeight + 100 });
+    Object.defineProperty(navigator, 'webdriver', { get: () => false });
+    Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });
+    Object.defineProperty(navigator, 'languages', { get: () => ['en'] });
+    Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
+    Object.defineProperty(navigator, 'hardwareConcurrency', { get: () => 8 });
+    Object.defineProperty(navigator, 'deviceMemory', { get: () => 8 });
+    Object.defineProperty(navigator, 'maxTouchPoints', { get: () => 1 });
+    Object.defineProperty(navigator, 'userAgent', {
+        get: () => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    });
 })();
